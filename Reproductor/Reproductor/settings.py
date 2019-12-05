3702222d8cd58c0ca652779c96fb2a55aee49b18
didiacosta <didi_acosta@hotmail.com>
2019-12-05 14:18:56 -0500

    consumo de API

--- Reproductor/Reproductor/settings.py
+++ Reproductor/Reproductor/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'Reproductor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,5 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2] + ['media'])
 MEDIA_URL = '/media/'
